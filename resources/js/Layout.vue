e4344e1b1d56fa67c7ecce78a95db5dc419d46be
Grant Holle <hollegrant@gmail.com>
2024-11-01 15:44:36 -0500

    Create project

---
+++ resources/js/Layout.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-col min-h-screen px-4">
+    <main class="flex-1 py-20">
+      <div class="rounded-xl bg-white/30 backdrop-blur-sm max-w-5xl mx-auto px-6 py-5 shadow-lg">
+        <slot />
+      </div>
+    </main>
+  </div>
+</template>
+
---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,10 @@
+<template>
+  <pre>{user:1}</pre>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({})
+const emit = defineEmits([])
+</script>
