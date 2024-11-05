1e58bc7fc1ebe4e149b104c7041e5a29af854f60
Grant Holle <hollegrant@gmail.com>
2024-11-04 21:45:09 -0600

    Add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Gilroy', ...defaultTheme.fontFamily.sans],
             },
         },
     },
